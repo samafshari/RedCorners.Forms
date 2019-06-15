2fcc33a3fbab4630156aef4995c1b2482a47b27d
Saeed Afshari (BLAZE) <s_a@live.com>
2019-06-15 14:23:45 +0200

    Some DelayedView

---
+++ RedCorners.Forms.Shared/Views/DelayedView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace RedCorners.Forms
+{
+    [ContentProperty("Content")]
+    public class DelayedView : ContentView2
+    {
+        public bool IsLoading { get; protected set; }
+
+        public View LoadingView
+        {
+            get => (View)GetValue(LoadingViewProperty);
+            set => SetValue(LoadingViewProperty, value);
+        }
+
+        public uint Delay
+        {
+            get => (uint)GetValue(DelayProperty);
+            set => SetValue(DelayProperty, value);
+        }
+
+        public static readonly BindableProperty LoadingViewProperty = BindableProperty.Create(
+            propertyName: nameof(LoadingView),
+            returnType: typeof(View),
+            declaringType: typeof(TitledContentView),
+            defaultValue: null,
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public static readonly BindableProperty DelayProperty = BindableProperty.Create(
+            propertyName: nameof(LoadingView),
+            returnType: typeof(uint),
+            declaringType: typeof(DelayedView),
+            defaultValue: 50,
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public DelayedView()
+        {
+        }
+
+        public override void OnStart()
+        {
+            base.OnStart();
+        }
+    }
+}
