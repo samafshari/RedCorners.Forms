5c360eca7fe83cbbf012c8a772f073745a121fc8
Saeed Afshari (BLAZE) <s_a@live.com>
2019-08-17 22:59:53 +0200

    5.27.0 Frame2 ShadowColor, ShadowRadius

---
+++ RedCorners.Demo.Shared/ViewModels/Frame2ViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using RedCorners.Forms;
+using RedCorners.Models;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace RedCorners.Demo.ViewModels
+{
+    public class Frame2ViewModel : BindableModel
+    {
+        public Frame2ViewModel()
+        {
+            Status = TaskStatuses.Success;
+        }
+
+        float _shadowRadius = 100.0f;
+        public double ShadowRadiusD
+        {
+            get => _shadowRadius;
+            set
+            {
+                _shadowRadius = (float)value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(ShadowRadius));
+            }
+        }
+
+        public float ShadowRadius
+        {
+            get => _shadowRadius;
+        }
+
+        double _hue = 50;
+        double _sat = 50;
+        double _lit = 50;
+        double _alpha = 100;
+
+        public Color ShadowColor => Color.FromHsla(_hue * 0.01, _sat * 0.01, _lit * 0.01, _alpha * 0.01);
+        public double Hue
+        {
+            get => _hue;
+            set
+            {
+                _hue = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(ShadowColor));
+            }
+        }
+
+        public double Sat
+        {
+            get => _sat;
+            set
+            {
+                _sat = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(ShadowColor));
+            }
+        }
+
+        public double Lit
+        {
+            get => _lit;
+            set
+            {
+                _lit = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(ShadowColor));
+            }
+        }
+
+        public double Alpha
+        {
+            get => _alpha;
+            set
+            {
+                _alpha = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(ShadowColor));
+            }
+        }
+    }
+}
---
+++ RedCorners.Forms.iOS/Renderers/Frame2Renderer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using Foundation;
+using UIKit;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Specifics = Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using SizeF = CoreGraphics.CGSize;
+using Xamarin.Forms;
+using RedCorners.Forms;
+using RedCorners.Forms.Renderers;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Frame2), typeof(Frame2Renderer))]
+namespace RedCorners.Forms.Renderers
+{
+    public class Frame2Renderer : VisualElementRenderer<Frame2>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame2> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+                SetupLayer();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == Xamarin.Forms.VisualElement.BackgroundColorProperty.PropertyName ||
+                e.PropertyName == Xamarin.Forms.Frame.BorderColorProperty.PropertyName ||
+                e.PropertyName == Xamarin.Forms.Frame.HasShadowProperty.PropertyName ||
+                e.PropertyName == Xamarin.Forms.Frame.CornerRadiusProperty.PropertyName ||
+                e.PropertyName == Frame2.ShadowRadiusProperty.PropertyName ||
+                e.PropertyName == Frame2.ShadowColorProperty.PropertyName)
+                SetupLayer();
+        }
+
+        void SetupLayer()
+        {
+            float cornerRadius = Element.CornerRadius;
+
+            if (cornerRadius == -1f)
+                cornerRadius = 5f; // default corner radius
+
+            var shadowRadius = Element.ShadowRadius;
+            if (shadowRadius == -1f)
+                shadowRadius = 5f;
+
+            Layer.CornerRadius = cornerRadius;
+
+            if (Element.BackgroundColor == Color.Default)
+                Layer.BackgroundColor = UIColor.White.CGColor;
+            else
+                Layer.BackgroundColor = Element.BackgroundColor.ToCGColor();
+
+            if (Element.HasShadow)
+            {
+                Layer.ShadowRadius = shadowRadius;
+                Layer.ShadowColor = Element.ShadowColor.ToCGColor();
+                Layer.ShadowOpacity = 1.0f;
+                Layer.ShadowOffset = new SizeF();
+            }
+            else
+                Layer.ShadowOpacity = 0;
+
+            if (Element.BorderColor == Color.Default)
+                Layer.BorderColor = UIColor.Clear.CGColor;
+            else
+            {
+                Layer.BorderColor = Element.BorderColor.ToCGColor();
+                Layer.BorderWidth = 1;
+            }
+
+            Layer.RasterizationScale = UIScreen.MainScreen.Scale;
+            Layer.ShouldRasterize = true;
+        }
+    }
+}
