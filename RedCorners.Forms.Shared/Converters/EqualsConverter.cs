e3d4f4d367ba214e67aff5bdea34baeb5140c22d
s_a@live.com (R) <s_a@live.com>
2020-11-12 17:47:18 +0200

    8.56.0 CountConverters

--- RedCorners.Forms.Shared/Converters/EqualsConverter.cs
+++ RedCorners.Forms.Shared/Converters/EqualsConverter.cs
@@ -1,32 +1,51 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
-
+using System.Linq;
 using Xamarin.Forms;
 
 namespace RedCorners.Forms.Converters
 {
-    public class EqualsConverter : IValueConverter
+    public class CountIsConverter : IValueConverter
     {
-        public static EqualsConverter Instance { get; } = new EqualsConverter();
+        public static CountIsConverter Instance { get; } = new CountIsConverter();
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value?.ToString() == parameter?.ToString();
+            int? p = null;
+            if (int.TryParse(parameter?.ToString(), out var i))
+                p = i;
+            if (p == null) return value == null;
+            if (value is IList e)
+                return e?.Count == p;
+            if (value is ICollection c)
+                return c?.Count == p;
+
+            return false;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
             null;
     }
 
-    public class NotEqualsConverter : IValueConverter
+    public class CountIsNotConverter : IValueConverter
     {
-        public static NotEqualsConverter Instance { get; } = new NotEqualsConverter();
+        public static CountIsNotConverter Instance { get; } = new CountIsNotConverter();
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value?.ToString() != parameter?.ToString();
+            int? p = null;
+            if (int.TryParse(parameter?.ToString(), out var i))
+                p = i;
+            if (p == null) return value != null;
+            if (value is IList e)
+                return e?.Count != p;
+            if (value is ICollection c)
+                return c?.Count != p;
+
+            return false;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
