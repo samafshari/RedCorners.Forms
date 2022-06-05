7ae53ff4b59eb89048fd982f6c44fe000a6cf378
s_a@live.com (NEATBOOK3) <s_a@live.com>
2022-06-05 17:34:45 +0200

    SwitchCommand v73

---
+++ RedCorners.Forms.Shared/SwitchCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace RedCorners.Forms
+{
+    public class SwitchCommand : BindableObject, ICommand
+    {
+        public static SwitchCommand Instance { get; } = new SwitchCommand();
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is Switch sw)
+                sw.IsToggled = !sw.IsToggled;
+        }
+    }
+}
